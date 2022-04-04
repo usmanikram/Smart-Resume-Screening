0175ca392b5b6ac57fffd9bd37a8ac1f12ea39c2
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-04-05 01:20:56 +0500

    Testing App Deleted and Template Linked with Views

--- SRS/SRS/urls.py
+++ SRS/SRS/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
