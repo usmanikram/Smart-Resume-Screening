32c594d761deef40f5d43159cc03cf32bef22d64
Najeebengr <najeeb.nedo5555@gmail.com>
2022-04-05 12:25:55 +0500

    Modified!

--- SRS/SRS/settings.py
+++ SRS/SRS/settings.py
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'base.apps.BaseConfig',
-=======
-    'testing.apps.TestingConfig',
->>>>>>> 0175ca392b5b6ac57fffd9bd37a8ac1f12ea39c2
 ]
 
 MIDDLEWARE = [
@@ -59,13 +55,7 @@ ROOT_URLCONF = 'SRS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
-=======
         'DIRS': [BASE_DIR / 'templates'],
->>>>>>> 0175ca392b5b6ac57fffd9bd37a8ac1f12ea39c2
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- SRS/SRS/urls.py
+++ SRS/SRS/urls.py
@@ -1,22 +1,8 @@
 
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls'))
-=======
-from django.urls import path
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return render(request,'index.html')
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home)
->>>>>>> 0175ca392b5b6ac57fffd9bd37a8ac1f12ea39c2
 ]
