c6574a85fcd261e08b385731fb7a6b8dd8f95a3f
ahmad <f2018266068@umt.edu.pk>
2022-05-23 11:42:48 +0500

    OK

--- SRS/base/urls.py
+++ SRS/base/urls.py
@@ -3,7 +3,7 @@ from .import views
 from django.shortcuts import render
 
 urlpatterns = [
-path('',views.home,name='home'),
-path('login',views.loginPage,name='login'),
-
+    path('',views.home, name="home"),
+    path('addUser/',views.ok , name="addUser"),
+    path('login/',views.loginPage , name="login"),
 ]
--- SRS/base/views.py
+++ SRS/base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,5 +6,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'base/home.html')
 
+def ok(request):
+    return render(request, 'base/addUser.html')
+
 def loginPage(request):
-    return render(request, 'base/login.html')
+    return render(request, 'base/login_register.html')
+
