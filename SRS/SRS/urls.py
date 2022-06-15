6a5ed19b0ae55880af57351c405495c623c2f6f4
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-06-16 01:46:38 +0500

    Revert "Merge Abrar Branch into Main Branch"

--- SRS/SRS/urls.py
+++ SRS/SRS/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
-    path('users/',include('users.urls')),
-    path('admindash/',include('adminpanel.urls'))
+    path('users/',include('users.urls'))
 ]
