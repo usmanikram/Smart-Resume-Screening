a3c870e1e89a9a37d6d025479d791f6065dc6bec
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-06-20 18:30:44 +0500

    Update urls.py

--- SRS/SRS/urls.py
+++ SRS/SRS/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
     path('users/',include('users.urls'),
-    path('admindash/',include('adminpanel.urls'))
+    path('admindash/',include('adminpanel.urls')),
 ]
