89177dd9c6df0caded9fd33e898464fb3deec5a7
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-06-21 06:17:28 +0500

    Fixed Signup and Login Screen Nav

--- SRS/base/views.py
+++ SRS/base/views.py
@@ -6,10 +6,12 @@ def home(request):
     return render(request, 'base/home.html')
   
 def signup(request):
-    return render(request, 'base/signup.html')
+    context = {"segment" : "Sign Up"}
+    return render(request, 'base/signup.html',context)
   
 def ok(request):
     return render(request, 'base/addUser.html')
 
 def login(request):
-    return render(request, 'base/login.html')
+    context = {"segment" : "Sign In"}
+    return render(request, 'base/login.html',context)
