4f707cf1070c86a067e0f32a7ee7679757d2bc91
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-13 02:26:35 +0500

    Admin Feedback View Created (#21)
    * Admin Feedback View Created
    
    * files upload to remove conflict
    
    * Delete SRS/adminpanel/templates/admin directory

--- SRS/adminpanel/views.py
+++ SRS/adminpanel/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def admindash(request):
     return render(request,'admindash.html')
-
+def feedback(request):
+    return render(request,'feedback.html')
 def reports(request):
     return render(request, 'reports.html')
 def finalreport(request):
