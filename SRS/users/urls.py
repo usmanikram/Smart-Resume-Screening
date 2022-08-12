a8004729f1c8bae6290ef5fd693182bca345b61b
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-13 01:55:12 +0500

    Main Merge (#16)
    * Fixed Image Issue
    
    * Revert "Merge Abrar Branch into Main Branch"
    
    * Conflict Resolve - Admin Dashboard
    
    * Revert "adminsidebar"
    
    * Admin Panel Addition
    
    * Update urls.py
    
    * Update settings.py
    
    * Footer and Admin Siderbar Nav Added
    
    * Update urls.py
    
    * Update urls.py
    
    * Update urls.py
    
    * Update views.py
    
    * Add files via upload
    
    * Added Footer
    
    * Small Fix (#15)
    
    * Fixed Signup and Login Screen Nav
    
    * Fixed Homepage Nav
    
    * Updated User and Admin Dashboard
    
    * Small Fix
    
    * Small Fix
    
    * Small Fix - Deleted Add Job Page
    
    * Update login.html
    
    Height set
    
    Co-authored-by: Najeebengr <94345811+Najeebengr@users.noreply.github.com>

--- SRS/users/urls.py
+++ SRS/users/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 path('',views.dashboard,name='dashboard'),
 path('editprofile',views.editprofile,name='editprofile'),
 path('jobs',views.jobs,name='jobs'),
-path('addjob',views.addjob,name='addjob'),
 path('feedback',views.feedback,name='feedback'),
 path('addfeedback',views.addfeedback,name='addfeedback'),
 path('reports',views.reports,name='reports'),
@@ -15,6 +14,8 @@ path('insights',views.insights,name='insights'),
 path('settings',views.settings,name='settings'),
 path('createjob',views.createjob, name='createjob'),
 path('viewjobs',views.viewjobs,name='viewjobs'),
+
 path('viewjobdetails',views.viewjobdetails,name='viewjobdetails'),
 path('editjob',views.editjob,name='editjob'),
 ]
+
