a4e89431eefae03d3cb13c7fd6fdc1f0ba3e3434
abrarRQ <arq.qadri@outlook.com>
2022-08-15 17:37:36 +0500

    Forms

---
+++ SRS/base/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import Users
+
+class SignupUserForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields='__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save person'))
