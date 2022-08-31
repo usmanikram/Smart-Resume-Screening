9486fd3f72240739c8f0ead7552ee8c4f76130a4
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-31 16:56:16 +0500

    Update forms.py

--- SRS/base/forms.py
+++ SRS/base/forms.py
@@ -1,15 +1,61 @@
-from django import forms
+from django.forms import ModelForm
+from .models import User,Job,Resume
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from .models import Users
+from django.forms import ClearableFileInput
 
-class SignupUserForm(forms.ModelForm):
+class UserForm(ModelForm):
     class Meta:
-        model = Users
-        fields='__all__'
+        model = User
+        fields = '__all__'
+        exclude = ('last_login','is_approved','is_staff','is_active',)
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Save person'))
+        self.helper.add_input(Submit('submit', 'Signup'))
+
+
+class SignupForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+        exclude = ('last_login','is_approved','is_staff','is_active',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Signup'))
+
+
+
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('email','password')
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Sign In'))
+
+
+class CreateJobForm(ModelForm):
+    class Meta:
+        model = Job
+        exclude = ('completion_date',)
+
+class CreateJobFormForResume(ModelForm):
+    class Meta:
+        model = Resume
+        exclude = ('job_id',)
+        widgets = {
+            'file_path': ClearableFileInput(attrs={'multiple': True}),
+        }
+
