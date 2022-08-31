4f1ca835957f3abf7e3085a36f3d4e89d69fe41a
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-31 17:04:44 +0500

    Update urls.py

--- SRS/users/urls.py
+++ SRS/users/urls.py
@@ -5,15 +5,19 @@ from django.shortcuts import render
 
 urlpatterns = [
 path('',views.dashboard,name='dashboard'),
+path('Logout', views.Logout, name="Logout"),
 path('editprofile',views.editprofile,name='editprofile'),
-path('jobs',views.jobs,name='jobs'),
+path('jobs/<str:pk>',views.jobs,name='jobs'),
 path('feedback',views.feedback,name='feedback'),
 path('addfeedback',views.addfeedback,name='addfeedback'),
 path('reports',views.reports,name='reports'),
 path('insights',views.insights,name='insights'),
 path('settings',views.settings,name='settings'),
 path('createjob',views.createjob, name='createjob'),
+path('viewjobs',views.viewjobs,name='viewjobs'),
 path('finalreport',views.finalreport,name='finalreport'),
+
+path('viewjobdetails/<str:pk>',views.viewjobdetails,name='viewjobdetails'),
 path('editjob',views.editjob,name='editjob'),
 ]
 
