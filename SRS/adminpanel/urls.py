a77a8d2126697ce5df2354f5bf438009bf407fc6
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-31 16:53:11 +0500

    Update urls.py

--- SRS/adminpanel/urls.py
+++ SRS/adminpanel/urls.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 
 urlpatterns = [
     path('',views.admindash,name='admindash'),
-    path('Userfeedback',views.Userfeedback,name='Userfeedback'),
-    path('Userreports',views.Userreports,name='Userreports'),
+    path('feedback',views.feedback,name='feedback'),
+    path('reports',views.reports,name='reports'),
     path('finalreport',views.finalreport,name='finalreport'),
     path('Users',views.Users,name='Users'),
     path('addUser',views.addUser,name='addUser'),
-    path('viewUser',views.viewUser,name='viewUser'),
+    path('viewUser/<str:pk>',views.viewUser,name='viewUser'),
     path('viewjobs',views.viewjobs,name='viewjobs'),
     path('viewjobdetails',views.viewjobdetails,name='viewjobdetails'),
   ]
