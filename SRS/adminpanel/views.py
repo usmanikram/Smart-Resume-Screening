97106143fc98ce6240173673e2b79954a5551156
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-08-31 16:54:53 +0500

    Update views.py

--- SRS/adminpanel/views.py
+++ SRS/adminpanel/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
+from base.models import User,Feedback,Report
+from .forms import addUserForm
 
 # Create your views here.
 def admindash(request):
     return render(request,'admindash.html')
-def Userfeedback(request):
-    return render(request,'userfeedback.html')
-def Userreports(request):
-    return render(request, 'userreports.html')
+def feedback(request):
+    obj = Feedback.objects.all()
+    context = {'obj': obj}
+    
+    return render(request,'Afeedback.html', context)
+def reports(request):
+    objR = Report.objects.all()
+    context ={'objR' : objR}
+
+    return render(request, 'Areports.html',context)
 def finalreport(request):
     return render(request, 'finalreport.html')
-def Users(request):
-    return render(request,'Users.html')
+def Users(request,):
+    object = User.objects.all()
+    return render(request,'Users.html',
+    {'object' : object})
 def addUser(request):
-    return render(request,'addUser.html')
-def viewUser(request):
-    return render(request,'viewUser.html')
-def viewjobs(request):
-    return render(request, 'viewjobs.html')
-def viewjobdetails(request):
-    return render(request, 'viewjobdetails.html')
+    form = addUserForm()
+    if request.method== 'POST':
+        form = addUserForm(request.POST)
+        if form.is_valid():
+
+            f = form.save(commit=False)
+            f.set_password(f.password)
+            f.save()
+
+            # messages.success(request, 'Account successfully created.')
+            # return redirect('')
+    context = {"segment" : "AddUser",'form':form}
+    return render(request, 'addUser.html',context)
+def viewUser(request, pk):
+    user = User.objects.get(id=pk)
+    context={'user':user}
+    return render(request,'viewUser.html', context)
 
