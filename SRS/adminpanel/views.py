ba147b702f0e8a23d6b2232227d51a4bedb6b90f
Muhammad Usman Ikram <usmanikram.ch@gmail.com>
2022-10-23 22:36:30 +0500

    Update views.py

--- SRS/adminpanel/views.py
+++ SRS/adminpanel/views.py
@@ -1,26 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from base.models import User,Feedback,Report,Job
 from .forms import addUserForm
 
 # Create your views here.
+@login_required(login_url='loginuser')
 def admindash(request):
     return render(request,'admindash.html')
+
+@login_required(login_url='loginuser')
 def feedback(request):
     obj = Feedback.objects.all()
     context = {'obj': obj}
     
     return render(request,'Afeedback.html', context)
+
+@login_required(login_url='loginuser')
 def reports(request):
     objR = Report.objects.all()
     context ={'objR' : objR}
 
     return render(request, 'Areports.html',context)
+
+@login_required(login_url='loginuser')
 def finalreport(request):
     return render(request, 'finalreport.html')
+
+@login_required(login_url='loginuser')
 def Users(request,):
-    object = User.objects.all()
+    object = User.objects.filter(is_staff = 'False', is_approved = 'True')
     return render(request,'Users.html',
     {'object' : object})
+
+@login_required(login_url='loginuser')
+def approveUser(request):
+    object = User.objects.filter(is_approved = 'False')
+    return render(request,'approveUser.html',
+    {'object' : object})
+
+@login_required(login_url='loginuser')
+def approveUserFunc(request, pk):
+    user = User.objects.get(id = pk)
+    user.is_approved = 'True'
+    user.save()
+    return redirect ('Users')
+
+@login_required(login_url='loginuser')
 def addUser(request):
     form = addUserForm()
     if request.method== 'POST':
@@ -35,31 +59,33 @@ def addUser(request):
             # return redirect('')
     context = {"segment" : "AddUser",'form':form}
     return render(request, 'addUser.html',context)
+
+@login_required(login_url='loginuser')
 def viewUser(request, pk):
     user = User.objects.get(id=pk)
     context={'user':user}
     return render(request,'viewUser.html', context)
 
-
+@login_required(login_url='loginuser')
 def deleteUser(request, pk):
     user = User.objects.get(id=pk)
     user.delete()
     return render(request,'admindash.html')
 
+@login_required(login_url='loginuser')
 def deleteJob(request, pk):
     job = Job.objects.get(id=pk)
     job.delete()
     return render(request,'admindash.html')
 
-
+@login_required(login_url='loginuser')
 def viewjobs(request):
     obj=Job.objects.all()
     return render(request, 'viewjobs.html',
     { 'obj' :obj})
     
-
+@login_required(login_url='loginuser')
 def viewjobdetails(request, pk):
     job= Job.objects.get(id=pk)
     context={'job':job}
-    return render(request, 'Aviewjobdetails.html',context)    
-
+    return render(request, 'Aviewjobdetails.html',context)   
